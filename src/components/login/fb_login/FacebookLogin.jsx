4e0c6b98f5530d4777065e2bdc735fb4a6f84048
colestanfield <cole.stanfield>
2022-11-21 21:49:20 -0700

    added facebook login component

---
+++ src/components/login/fb_login/FacebookLogin.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+import FacebookLogin from "react-facebook-login";
+
+const ReactFacebookLogin = () => {
+    const [accessToken, setAccessToken] = useState("");
+
+    const componentClicked = data => {
+        console.log("data", data);
+    };
+
+    const responseFacebook = response => {
+        // console.log(response.accessToken);
+        setAccessToken(response.accessToken);
+    };
+
+    return (
+        <div>
+            React Facebook Login
+            <br />
+            User Short-Lived Access Token:
+            <br />
+            {accessToken}
+            <br />
+            <FacebookLogin
+                appId="1614986128648687"
+                autoLoad={true}
+                fields="name,email,picture"
+                onClick={componentClicked}
+                callback={responseFacebook}
+            />
+        </div>
+    );
+};
