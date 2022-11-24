10cce66498e5cb11b19c30bd5435cc3008707997
colestanfield <cole.stanfield>
2022-11-23 18:05:12 -0700

    More logs

--- src/services/account.service.js
+++ src/services/account.service.js
@@ -22,10 +22,11 @@ export const accountService = {
 async function login() {
     // login with facebook then authenticate with the API to get a JWT auth token
     console.log("doggy 1")
-    const { authResponse } = await new Promise(window.FB.login);
-    if (!authResponse) return;
+    const response = await new Promise(window.FB.login);
+    console.log(response)
+    if (!response) return;
 
-    await apiAuthenticate(authResponse.accessToken);
+    // await apiAuthenticate(authResponse.accessToken);
 
     // get return url from location state or default to home page
     const { from } = history.location.state || { from: { pathname: "/" } };
