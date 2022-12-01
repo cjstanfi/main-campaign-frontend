fd32b468aba227206f13eb8a2d05cc0eef5587dd
colestanfield <cole.stanfield>
2022-12-01 11:42:30 -0700

    made axios call async and tied it to use effect react hook

--- src/components/MainDashboard/MainDashboard.jsx
+++ src/components/MainDashboard/MainDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import BarChartRate from "./BarChartRate";
 import GeoChartRate from "./GeoChartRate";
 
@@ -7,35 +7,44 @@ import TopNav from "./TopNav";
 import TotalCount from "./TotalCount";
 
 import axios from 'axios';
+import {history, initFacebookSdk} from "../../helpers";
+import {accountService} from "../../services";
 
 
 export default function MainDashboard() {
   const [navistoggled, setnavistoggled] = useState(false);
+  useEffect( () => {
+    async function sendZohoTokens(){
+        // redirect to home if already logged in
+        const queryString = window.location.search
+        const urlParams = new URLSearchParams(queryString)
+        console.log(queryString)
+        console.log(urlParams)
+        if(urlParams.has('code')) {
+            const code = urlParams.get('code')
+            const location = urlParams.get('location') //save to backend for access and refresh tokens
 
-  const queryString = window.location.search
-  const urlParams = new URLSearchParams(queryString)
-    console.log(queryString)
-    console.log(urlParams)
-    if(urlParams.has('code')) {
-        const code = urlParams.get('code')
-        const location = urlParams.get('location') //save to backend for access and refresh tokens
+            //there are more of these
+            const domainSpecificUrl = location === "us" ? "https://accounts.zoho.com" : null
 
-        //there are more of these
-        const domainSpecificUrl = location === "us" ? "https://accounts.zoho.com" : null
+            const result = await axios.post('https://wwww.api.maincampaign.com/zoho-access-token',
+                {
+                    code,
+                    domainSpecificUrl
+                })
+            console.log(result)
 
-        axios.post('https://wwww.api.maincampaign.com/zoho-access-token',
-            {
-                code,
-                domainSpecificUrl
-            })
-
-        // axios.get(
-        //     "https://profile.zoho.com/api/v1/user/self/profile",
-        //     { headers: {
-        //         'Authorization': `Zoho-oauthtoken ${response.access_token}`
-        //       }}
-        // ).then()
+            // axios.get(
+            //     "https://profile.zoho.com/api/v1/user/self/profile",
+            //     { headers: {
+            //         'Authorization': `Zoho-oauthtoken ${response.access_token}`
+            //       }}
+            // ).then()
+        }
     }
+    sendZohoTokens()
+    });
+
 
   function clickEvent() {
     setnavistoggled(!navistoggled);
