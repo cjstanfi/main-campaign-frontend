01f3594d7b8b2ef21b5d6f10bb1a1288634ed95b
RTorgeso1 <112680283+RTorgeso1@users.noreply.github.com>
2022-11-30 17:12:28 -0700

    access token save moved to backend

--- src/components/MainDashboard/MainDashboard.jsx
+++ src/components/MainDashboard/MainDashboard.jsx
@@ -19,40 +19,15 @@ export default function MainDashboard() {
     if(urlParams.has('code')) {
         const code = urlParams.get('code')
         const location = urlParams.get('location') //save to backend for access and refresh tokens
-        const accountsServer = urlParams.get('accounts-server')
 
         //there are more of these
         const domainSpecificUrl = location === "us" ? "https://accounts.zoho.com" : null
 
-        let formData = new FormData();
-        formData.append('grant_type', "authorization_code")
-        formData.append('client_id', "1000.TO2UPAI0LXK9RRU0HYS5ZQ1OZ8UPBI")
-        formData.append('client_secret', "650254d3a45ed3113b0580863453e1e5af6cb2e9db")
-        formData.append('redirect_uri', "https://www.maincampaign.com/dashboard")
-        formData.append('code', code)
-
-        //get access_token and refresh_token
-        axios.post(`${domainSpecificUrl}/oauth/v2/token`,
-            formData,
-            { headers: { 'content-type': 'multipart/form-data' } }
-        ).then(tokenResponse => {
-            console.log(tokenResponse)
-                //get the current zoho user
-                axios.get("https://www.zohoapis.com/crm/v3/users?type=CurrentUser&scope=ZohoCRM.users.READ",
-                    { headers: { 'Authorization': "Zoho-oauthtoken " + tokenResponse.access_token } }
-                ).then(userResponse => {
-                    console.log(tokenResponse)
-                    //save user in backend
-                    axios.put(`https://wwww.maincampaign.com/sales-crm-account/${userResponse.id}`,
-                    {
-                        salesCrmAccountAccessToken: tokenResponse.access_token,
-                        salesCrmAccountDomainUrl: accountsServer,
-                        salesCrmAccountRefreshToken: tokenResponse.refresh_token,
-                        salesCrmAccountId: userResponse.id,
-                    }
-                )
+        axios.post('https://wwww.maincampaign.com/zoho-access-token',
+            {
+                code,
+                domainSpecificUrl
             })
-        })
 
         // axios.get(
         //     "https://profile.zoho.com/api/v1/user/self/profile",
