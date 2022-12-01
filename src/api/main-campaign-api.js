02e282caa3113708ef7c911ee047d7a870b8ded1
RTorgeso1 <112680283+RTorgeso1@users.noreply.github.com>
2022-12-01 13:27:43 -0700

    getAnything first test

--- src/api/main-campaign-api.js
+++ src/api/main-campaign-api.js
@@ -1,3 +1,5 @@
-export function getAnything(){
+import axios from "axios";
 
+export async function getAnything(id) {
+    await axios.get(`https://test.api.maincampaign.com/facebook-ad-set-daily-insight/${id}`)
 }
