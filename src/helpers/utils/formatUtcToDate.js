d8adcf4325492249aee21261c015cdaa52362ddd
RTorgeso1 <112680283+RTorgeso1@users.noreply.github.com>
2022-12-27 14:51:47 -0700

    actual formatter

--- src/helpers/utils/formatUtcToDate.js
+++ src/helpers/utils/formatUtcToDate.js
@@ -1,3 +1,11 @@
 export function formatUtcToDate(date) {
-    return date
+    const newDate = new Date(date)
+    const yyyy = newDate.getFullYear();
+    let mm = newDate.getMonth() + 1; // Months start at 0!
+    let dd = newDate.getDate();
+
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+
+    return dd + '/' + mm + '/' + yyyy
 }
